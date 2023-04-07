de0bedb8c7a044ebfe539db6e3fb9366f0e69a63
alexanderosadc <alexanderosadc@gmail.com>
2023-04-07 18:56:33 +0300

    feat: scraper for imsdb.com

--- main.go
+++ main.go
@@ -3,9 +3,5 @@ package main
 import "github.com/alexanderosadc/go-web-app/pkg/scraper"
 
 func main() {
-	var selectors :=[]string{
-		
-	}
-
-	scraper.ScrapeWebPage("https://quotes.toscrape.com/", selectors)
+	scraper.ScrapeWebPage("https://imsdb.com")
 }
--- pkg/scraper/scraper.go
+++ pkg/scraper/scraper.go
@@ -1,27 +1,56 @@
 package scraper
 
 import (
-	"fmt"
+    "fmt"
+    "log"
 
-	"github.com/gocolly/colly"
+    "github.com/gocolly/colly"
 )
 
-func ScrapeWebPage(url string, cssSelectors []string) {
-	c := colly.NewCollector()
+func ScrapeWebPage(url string) {
+    c := colly.NewCollector()
+    movieLinksCol := c.Clone()
+    movieScriptsLinkCol := c.Clone()
+    movieScriptCol := c.Clone()
+   
+    attribute := "a[href^=\"/alphabetical/" + "\"]"
+    c.OnHTML(attribute, func(e *colly.HTMLElement){
+        link := e.Attr("href")
+        fmt.Println(e.Text)
+        if err := movieLinksCol.Visit(url + link); err != nil{
+            fmt.Println(err)
+        }})
 
-	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("Visiting", r.URL)
-	})
 
-	for _, elem := range cssSelectors {
-		c.OnHTML(elem, func(e *colly.HTMLElement) {
-			fmt.Println(e.Text)
-		})
-	}
+    attribute = "p > a[href^=\"/Movie Scripts/\"]"
+    fmt.Println(attribute)
 
-	c.OnScraped(func(r *colly.Response) {
-		fmt.Println("Finished", r.Request.URL)
-	})
+     movieLinksCol.OnHTML(attribute, func(e *colly.HTMLElement){
+        link := e.Attr("href")
+        fmt.Println(url + link)
+        if err := movieScriptsLinkCol.Visit(url + link); err != nil{
+            fmt.Println(err)
+        }
+    })
 
-	c.Visit(url)
+    attribute = "td > a[href^=\"/scripts/\"]"
+    movieScriptsLinkCol.OnHTML(attribute, func(e *colly.HTMLElement){
+        link := e.Attr("href")
+        fmt.Println(link)
+        movieScriptCol.Visit(url + link)
+    })
+
+    attribute = "td.scrtext > pre"
+    movieScriptCol.OnHTML(attribute, func(e *colly.HTMLElement){
+        text := e.Text
+        fmt.Println(text)
+    })
+
+    c.OnScraped(func(r *colly.Response) {
+        fmt.Println("Finished", r.Request.URL)
+    })
+
+    if err := c.Visit(url); err != nil{
+        log.Fatalf("Error visiting web page: %s | error: %s", url, err)
+    }
 }
