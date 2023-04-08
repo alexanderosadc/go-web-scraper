b7d666bfd8a47ecde9e2b64bf9d5de0a074efcae
alexanderosadc <alexanderosadc@gmail.com>
2023-04-08 17:05:48 +0300

    refactor: creates function for crawling links

--- pkg/scraper/scraper.go
+++ pkg/scraper/scraper.go
@@ -1,68 +1,66 @@
 package scraper
 
 import (
-    "fmt"
-    "log"
-    "os"
+	"fmt"
+	"log"
+	"os"
 
-    "github.com/gocolly/colly"
+	"github.com/gocolly/colly"
 )
 
 func ScrapeWebPage(url string) {
-    colly.CacheDir("./cache/")
     c := colly.NewCollector()
-    movieLinksCol := c.Clone()
-    movieScriptsLinkCol := c.Clone()
-    movieScriptCol := c.Clone()
-   
-    attribute := "a[href^=\"/alphabetical/" + "\"]"
-    c.OnHTML(attribute, func(e *colly.HTMLElement){
-        link := e.Attr("href")
-        fmt.Println(e.Text)
-        if err := movieLinksCol.Visit(url + link); err != nil{
-            fmt.Println(err)
-        }})
+    attributes := []string{"a[href^=\"/alphabetical/" + "\"]", "p > a[href^=\"/Movie Scripts/\"]", "td > a[href^=\"/scripts/\"]"}
+    var nextCol *colly.Collector
 
-
-    attribute = "p > a[href^=\"/Movie Scripts/\"]"
-     movieLinksCol.OnHTML(attribute, func(e *colly.HTMLElement){
-        link := e.Attr("href")
-        if err := movieScriptsLinkCol.Visit(url + link); err != nil{
-            fmt.Println(err)
-        }
-    })
-
-    attribute = "td > a[href^=\"/scripts/\"]"
-    movieScriptsLinkCol.OnHTML(attribute, func(e *colly.HTMLElement){
-        link := e.Attr("href")
-        if err := movieScriptCol.Visit(url + link); err != nil{
-            fmt.Println(err)
-        }
-    })
-
-    attribute = "td.scrtext > pre"
-    movieScriptCol.OnHTML(attribute, func(e *colly.HTMLElement){
-        _ = e.DOM.Find("b").Remove()
-        res, err :=e.DOM.Html()
-        if err != nil{
-            fmt.Println(err)
-        }
-
-        f, err := os.Create("." + e.Request.URL.Path)
-        if err != nil{
-            fmt.Println(err) 
-        }
-        defer f.Close()
-
-        nrByte, err := f.Write([]byte(res))
-        if err != nil{
-            fmt.Println(err)
-        }
-
-        fmt.Printf("nr of bytes written:%b, filename:%s\n", nrByte, e.Request.URL.Path)
-    })
-
-    if err := c.Visit(url); err != nil{
-        log.Fatalf("Error visiting web page: %s | error: %s", url, err)
+    for _, attribute := range(attributes){
+        nextCol = crawl(attribute, url)
+        fmt.Printf("after crawl %#v \n", nextCol)
     }
+
+    if nextCol == nil{
+        log.Println("nextCol == nil")
+        return
+    }
+
+	attribute := "td.scrtext > pre"
+	nextCol.OnHTML(attribute, func(e *colly.HTMLElement) {
+		_ = e.DOM.Find("b").Remove()
+		res, err := e.DOM.Html()
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		f, err := os.Create("." + e.Request.URL.Path)
+		if err != nil {
+			fmt.Println(err)
+		}
+		defer f.Close()
+
+		nrByte, err := f.Write([]byte(res))
+		if err != nil {
+			fmt.Println(err)
+		}
+
+		fmt.Printf("nr of bytes written:%b, filename:%s\n", nrByte, e.Request.URL.Path)
+	})
+
+	if err := c.Visit(url); err != nil {
+		log.Fatalf("Error visiting web page: %s | error: %s", url, err)
+	}
+}
+
+func crawl(attribute string, url string) (nextCol *colly.Collector) {
+	c := colly.NewCollector()
+	c.OnHTML(attribute, func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		fmt.Println(e.Text)
+		nextCol = c.Clone()
+		if err := nextCol.Visit(url + link); err != nil {
+			log.Println(err)
+            nextCol = nil
+            return
+		}
+	})
+	return
 }
